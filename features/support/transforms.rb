f0839b5ad5343ea5e692a972f26f37c6a1c8088c
Jason Phebus <jason.phebus@vitalsource.com>
2018-10-12 10:22:51 -0400

    Remove active support dependency Update to cucumber 3 Monkeypatches for Hash to do work done previously by active support (from_xml and symbolize_keys) Remove transforms (cuke 3)

--- features/support/transforms.rb
+++ features/support/transforms.rb
@@ -1,32 +1,5 @@
-# Replaces pattern with string substitutions described in Framework::World
-Transform /:unique_id|:now/ do |step|
-  replace = replaceable.select { |r| step.include?(r) }
-  replace_variables(step, substitutions(replace))
-end
-
-# Gets a table as its input, does replacements
-Transform /^table:/ do |table|
-  # Map over the rows, columns substituting values and return the Ast::Table object.
-  # Cucumber::Ast::Table.new(table.raw.map { |row| row.map { |col| col.multi_gsub(substitutions) } })
-
-  # Have to transpose the table during this section so the mapping works correctly
-  trans_table = table.transpose
-
-  # Only replace strings that exist in the table, don't run substitutions on non-existent strings. Also these replacements
-  # only work on 2 column tables (as they are meant for parameter->value replacements)
-  if table.columns.length == 2
-    replace = table.rows_hash.values.map { |value|
-      replaceable.select { |r| value.include?(r) }.last
-    }.compact
-  end
-
-  # Don't replace if nothing is replaceable.
-  unless replace.to_s.empty?
-    trans_table.column_names.each { |column_name|
-      trans_table.map_column!(column_name) { |col|
-        replace_variables(col, substitutions(replace))
-      }
-    }
-  end
-  trans_table.transpose
-end
+ParameterType(
+  name: 'unique_id',
+  regexp: /:unique_id/,
+  transformer: ->(name) { replace_variables(name) }
+)
